2bbc9ffe781e1585d11195dd48061a31f3114c07
atommarvel <atom.raiff@gmail.com>
2021-02-01 19:59:44 -0500

    update AlarmExperienceScreen.kt

--- app/src/main/java/com/radiantmood/calarm/screen/AlarmExperienceScreen.kt
+++ app/src/main/java/com/radiantmood/calarm/screen/AlarmExperienceScreen.kt
@@ -2,18 +2,28 @@ package com.radiantmood.calarm.screen
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.radiantmood.calarm.util.Fullscreen
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAlarmExperienceScreen() {
+    AlarmExperienceScreen(title = "Alarm", stopExperience = { /*TODO*/ })
+}
 
 @Composable
 fun AlarmExperienceScreen(title: String, stopExperience: () -> Unit) {
     Fullscreen {
-        Text(title)
+        Text(title, fontSize = 32.sp, textAlign = TextAlign.Center)
         Spacer(modifier = Modifier.height(24.dp))
-        Button(stopExperience) { Text("Stop") }
+        Button(onClick = stopExperience) { Text("Stop", modifier = Modifier.padding(40.dp), fontSize = 24.sp) }
     }
 }
