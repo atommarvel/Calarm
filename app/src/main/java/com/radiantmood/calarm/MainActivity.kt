1b20adc157740ee8937549aebd700c79d3a7339a
atommarvel <atom.raiff@gmail.com>
2021-02-10 01:23:35 -0500

    simplify MainActivity.kt

--- app/src/main/java/com/radiantmood/calarm/MainActivity.kt
+++ app/src/main/java/com/radiantmood/calarm/MainActivity.kt
@@ -1,31 +1,13 @@
 package com.radiantmood.calarm
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.radiantmood.calarm.util.TAG
 import com.radiantmood.calarm.util.render
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (intent.getBooleanExtra("isAlarm", false)) {
-            Log.d(TAG, "onCreate: alarm!")
-            var windowFlags = WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-                setTurnScreenOn(true)
-                setShowWhenLocked(true)
-            } else {
-                windowFlags = windowFlags or
-                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
-                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
-                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-            }
-            window.addFlags(windowFlags)
-        }
         lifecycleScope.launchWhenResumed {
             render {
                 App(this@MainActivity)
