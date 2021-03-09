45a79bc8fb1f9f4c8c168b025774bf836fdf5c10
atommarvel <atom.raiff@gmail.com>
2021-03-09 02:26:12 -0500

    polish settings screen

--- app/src/main/java/com/radiantmood/calarm/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/radiantmood/calarm/screen/settings/SettingsScreen.kt
@@ -1,30 +1,31 @@
 package com.radiantmood.calarm.screen.settings
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import com.radiantmood.calarm.CalendarSelectionScreen
 import com.radiantmood.calarm.LocalAppBarTitle
 import com.radiantmood.calarm.LocalNavController
 import com.radiantmood.calarm.compose.CalarmTopAppBar
 import com.radiantmood.calarm.compose.ModelContainerContent
-import com.radiantmood.calarm.compose.SectionTitle
 import com.radiantmood.calarm.navigate
 import com.radiantmood.calarm.screen.LoadingModelContainer
 import com.radiantmood.calarm.screen.ModelContainer
@@ -58,30 +59,73 @@ fun SettingsScreen() {
 
 @Composable
 fun SettingsList(screenModel: SettingsScreenModel) {
-    val navController = LocalNavController.current
-    val hasSelectedCalendars = screenModel.selectedCalendars.isNotEmpty()
+    // show expanded by default if there are less than 5 selected calendars
+    val (expanded, setExpanded) = remember { mutableStateOf(screenModel.selectedCalendars.size < 5) }
     LazyColumn {
-        SectionTitle(hasSelectedCalendars, "Currently Selected Calendars")
-        if (hasSelectedCalendars) item { Spacer(modifier = Modifier.size(16.dp)) }
-        ViewCalendarsRow(navController, hasSelectedCalendars)
-        CalendarsList(screenModel.selectedCalendars)
+        item {
+            ViewCalendarsRow()
+            Divider()
+        }
+        item {
+            SelectedCalendarsHeader(screenModel.selectedCalendars.isNotEmpty(), expanded, setExpanded)
+        }
+        SelectedCalendars(expanded, screenModel.selectedCalendars)
     }
 }
 
+@Composable
+fun SelectedCalendarsHeader(shouldShow: Boolean, expanded: Boolean, setExpanded: (Boolean) -> Unit) {
+    if (shouldShow) {
+        val expandIconRotation = animateFloatAsState(targetValue = if (expanded) 0f else 180f)
+        SettingsRow(icon = Icons.Default.ExpandLess, iconRotation = expandIconRotation.value, text = "Currently selected calendars:") {
+            setExpanded(expanded)
+        }
+        Divider()
+    }
+}
 
-fun LazyListScope.ViewCalendarsRow(navController: NavController, hasSelectedCalendars: Boolean = false) {
-    val label = if (hasSelectedCalendars) "Select More Calendars" else "Select Calendars" // TODO: to vm
-    item {
-        // TODO: make a settings row
-        Button(onClick = { navController.navigate(CalendarSelectionScreen) }) {
-            Text(label, modifier = Modifier.padding(12.dp))
+fun LazyListScope.SelectedCalendars(shouldShow: Boolean, calendars: List<CalendarSelectionModel>) {
+    if (shouldShow) {
+        items(calendars) { calendar ->
+            Box(modifier = Modifier.padding(start = 24.dp)) {
+                CalendarRow(calendar)
+            }
+            Divider()
         }
     }
 }
 
-fun LazyListScope.CalendarsList(calendars: List<CalendarSelectionModel>) {
-    items(calendars) { calendar ->
-        CalendarRow(calendar)
-        Divider()
+@Composable
+fun ViewCalendarsRow() {
+    val navController = LocalNavController.current
+    SettingsRow(icon = Icons.Default.CalendarToday, contentDescription = "Calendar", text = "Add / remove calendars") {
+        navController.navigate(CalendarSelectionScreen)
+    }
+}
+
+@Composable
+fun SettingsRow(
+    icon: ImageVector,
+    contentDescription: String? = null,
+    iconRotation: Float? = null,
+    text: String,
+    textStyle: TextStyle = TextStyle.Default,
+    onClick: (() -> Unit)? = null
+) {
+    val modifier = onClick?.let { Modifier.clickable { it.invoke() } } ?: Modifier
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val iconMod = iconRotation?.let {
+            Modifier.graphicsLayer {
+                rotationZ = it
+            }
+        } ?: Modifier
+        Icon(imageVector = icon, modifier = iconMod, contentDescription = contentDescription)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text, style = textStyle)
     }
 }
