59c9d2736da247533b1dbf8d7dfa809f4fe5a122
atommarvel <atom.raiff@gmail.com>
2021-05-08 15:41:02 -0400

    revert small shape definition

--- app/src/main/java/com/radiantmood/calarm/ui/theme/Shape.kt
+++ app/src/main/java/com/radiantmood/calarm/ui/theme/Shape.kt
@@ -5,7 +5,7 @@ import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
 val shapes = Shapes(
-    small = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+    small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(12.dp),
     large = RoundedCornerShape(0.dp)
 )
